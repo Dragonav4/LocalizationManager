93d463065be8e4754d287635a6b04f07532189a1
dragonav <daniliandan@gmail.com>
2025-08-12 19:20:39 +0200

    Added DataLayer

---  LocalizationManager.Web/DataLayer/Models/CultureEntity.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace LocalizationManager.Web.DataLayer.Models;
-
-public class CultureEntity
-{
-    [Key] public string Code { get; set; } = null!; // en-US
-    [Required] public string DisplayName { get; set; } = null!;
-}
